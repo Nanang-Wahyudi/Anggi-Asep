3115d2287a0ab80c60698c4f7c157b0af6c663fc
Nanang-Wahyudi <nanang.wahyudi220503@gmail.com>
2023-05-28 16:18:34 +0700

    modif: custom.js

--- custom.js
+++ custom.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Menampilkan nilai parameter di dalam elemen HTML
         document.getElementById('result').innerHTML = `
-          <p>Name: ${nameParam}</p>
+          <h1>${nameParam} And Partner</h1>
         `;
       });
